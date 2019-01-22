93ad3dfff55a74a943310ca5389d3b5a86e9aba3
mmujumdar22 <46583770+mmujumdar22@users.noreply.github.com>
2019-01-22 16:41:58 -0600

    Add files via upload

---
+++ potd-q1/hello.cpp
@@ -0,0 +1,9 @@
+/* Your code here! */
+//using namespace std;
+
+//std::string hello()
+
+// int your_age = 19;
+// string your_name = "Matt";
+//  string answer = "Hello world! My name is " + your_name + " and I am " + your_age + " years old.";
+//  return answer;
