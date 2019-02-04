349352015866b4fd2a727787c919e41433e70e53
mmujumdar22 <46583770+mmujumdar22@users.noreply.github.com>
2019-02-04 16:45:23 -0600

    Add files via upload

--- mp_stickers/Image.h
+++ mp_stickers/Image.h
@@ -4,4 +4,23 @@
  */
 
 #pragma once
+#ifndef IMAGE_H
+#define IMAGE_H
 
+#include "png.h"
+
+class Image : public PNG
+{
+  void lighten();
+  void darken();
+  void darken(double amount);
+  void saturate();
+  void saturate(double amount);
+  void desaturate();
+  void greyscale();
+  void rotateColor(double degrees);
+  void illinify();
+  void scale(double factor);
+  void scale(unsigned w, unsinged h);
+}
+#endif
