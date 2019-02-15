b869b5efe4eeefaf2035eb21ae78fe05158c68a4
mmujumdar22 <46583770+mmujumdar22@users.noreply.github.com>
2019-02-14 22:09:00 -0600

    Add files via upload

--- potd-q18/Node.cpp
+++ potd-q18/Node.cpp
@@ -1,8 +1,52 @@
 #include "Node.h"
+#include <iostream>
+
 
 void mergeList(Node *first, Node *second) {
 
-		
+	//std::cout << __LINE__ << std::endl;
+
+	if(first == NULL && second == NULL)
+		return;
+		//std::cout << __LINE__ << std::endl;
+	if(first != NULL || second != NULL)
+	{
+		//std::cout << __LINE__ << std::endl;	
+		Node *firstTemp = NULL;
+		Node *secondTemp = NULL;
+		//std::cout << __LINE__ << std::endl;
+		if(first != NULL && first->next_ != NULL)
+		{
+			firstTemp = first->next_;
+			if(second != NULL)
+				first->next_ = second;
+		}
+		else if(first != NULL)
+		{
+			first->next_ = second;
+			return;
+		}
+		//std::cout << __LINE__ << std::endl;		
+		if(second != NULL && second->next_ != NULL)
+		{
+			secondTemp = second->next_;
+			if(first != NULL)
+				second->next_ = firstTemp;
+		}
+		// else if(second != NULL)
+		// {
+		// 	first->next_ = second;
+		// }
+		//std::cout << __LINE__ << std::endl;		
+
+		//std::cout << __LINE__ << std::endl;
+		//second->next_ = firstTemp;
+		//std::cout << second->next_->data_ << " " << secondTemp->next_->data_<< std::endl;
+		mergeList(firstTemp, secondTemp);
+		//std::cout << __LINE__ << std::endl;
+	}
+	else
+		return;	
   // your code here!
 }
 
